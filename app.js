56a87037e5e901c2fbd8806f71ea739581ec9379
kuchipatchi09 <cnsh32_1218@cnsh.hs.kr>
2026-08-29 12:46:08 +0900

    Refactor Firebase imports in app.js
    Updated Firebase imports to use version 10.12.2 and removed GoogleAuthProvider.

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 import {
   auth,
   db,
-  googleProvider
+  provider
 } from "./firebase-config.js";
 
-
-
 import {
-  GoogleAuthProvider,
   signInWithPopup,
   signOut,
   onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 import {
   collection,
@@ -20,7 +17,7 @@ import {
   onSnapshot,
   setDoc,
   serverTimestamp
-} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 
 const SCHOOL_DOMAIN = "g.cnees.kr";
